9648f5c4eacfa41d5bd1606324f78c1f83a7feff
mahjoub <djot_04@yahoo.fr>
2021-01-02 15:44:44 +0100

    page principale de site master.blade.php

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>laravel Site Ecomerce</title>
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Raleway:100,600">
+</head>
+<body>
+    <div class="flex-center position-ref full-height">
+        <h1>Ceci est contenu dans master</h1>
+        @yield('content')
+    </div>
+</body>
+</html>
