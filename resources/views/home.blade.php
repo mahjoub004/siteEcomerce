f01e2fde488c20a45543512226aa060d694a2898
mahjoub <djot_04@yahoo.fr>
2021-01-02 15:58:13 +0100

    mise en place de la page master de site et l'essai de fonctionnement de site avec la page home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+@section('content')
+<h1>bonjour le monde </h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti, omnis cum facilis fugit qui enim laudantium unde dolores explicabo aperiam neque ea? Cumque et aperiam ipsum odit sapiente aut optio!</p>
+@endsection
