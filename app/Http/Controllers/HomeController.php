69568b36d409b691fe85ebb691cbdf175cb8c1a4
mahjoub <djot_04@yahoo.fr>
2021-01-04 15:14:09 +0100

    la création de page checkout.blade.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,6 +30,14 @@ class HomeController extends Controller
     {
         return view('cart');
     }
+    public function checkout()
+    {
+        return view('checkout');
+    }
+    public function success()
+    {
+        return view('success');
+    }
 
     public function product()
     {
