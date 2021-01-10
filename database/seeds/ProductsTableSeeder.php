efa51684b83a3152e891974c925ffee009f42e9a
mahjoub <djot_04@yahoo.fr>
2021-01-10 12:06:11 +0100

    modifier created par create

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -12,7 +12,7 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        Product::created([
+        Product::create([
             'name' => 'Mangue / Passion',
             'slug' => 'mangue-passion',
             'details' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, numquam quia, voluptate mollitia nam dignissimos amet est tempore expedita illum ipsa delectus magni provident! Dignissimos autem vero itaque debitis aliquam.',
@@ -22,7 +22,7 @@ class ProductsTableSeeder extends Seeder
             Accusamus voluptatem ipsum veritatis fugiat eaque, facere qui nobis veniam sunt unde nulla rem obcaecati, sit repellendus. Est, suscipit voluptatibus earum cupiditate nihil qui labore? Molestias minus omnis velit odio!',
         ]);
 
-        Product::created([
+        Product::create([
             'name' => 'Mangue / Fraise',
             'slug' => 'mangue-fraise',
             'details' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, numquam quia, voluptate mollitia nam dignissimos amet est tempore expedita illum ipsa delectus magni provident! Dignissimos autem vero itaque debitis aliquam.',
@@ -32,7 +32,7 @@ class ProductsTableSeeder extends Seeder
             Accusamus voluptatem ipsum veritatis fugiat eaque, facere qui nobis veniam sunt unde nulla rem obcaecati, sit repellendus. Est, suscipit voluptatibus earum cupiditate nihil qui labore? Molestias minus omnis velit odio!',
         ]);
 
-        Product::created([
+        Product::create([
             'name' => 'Mangue /Cerise',
             'slug' => 'mangue-cerise',
             'details' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, numquam quia, voluptate mollitia nam dignissimos amet est tempore expedita illum ipsa delectus magni provident! Dignissimos autem vero itaque debitis aliquam.',
@@ -42,7 +42,7 @@ class ProductsTableSeeder extends Seeder
             Accusamus voluptatem ipsum veritatis fugiat eaque, facere qui nobis veniam sunt unde nulla rem obcaecati, sit repellendus. Est, suscipit voluptatibus earum cupiditate nihil qui labore? Molestias minus omnis velit odio!',
         ]);
 
-        Product::created([
+        Product::create([
             'name' => 'Mangue / Orange',
             'slug' => 'mangue-Orange',
             'details' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, numquam quia, voluptate mollitia nam dignissimos amet est tempore expedita illum ipsa delectus magni provident! Dignissimos autem vero itaque debitis aliquam.',
@@ -52,7 +52,7 @@ class ProductsTableSeeder extends Seeder
             Accusamus voluptatem ipsum veritatis fugiat eaque, facere qui nobis veniam sunt unde nulla rem obcaecati, sit repellendus. Est, suscipit voluptatibus earum cupiditate nihil qui labore? Molestias minus omnis velit odio!',
         ]);
 
-        Product::created([
+        Product::create([
             'name' => 'Mangue / Poire',
             'slug' => 'mangue-Poire',
             'details' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, numquam quia, voluptate mollitia nam dignissimos amet est tempore expedita illum ipsa delectus magni provident! Dignissimos autem vero itaque debitis aliquam.',
@@ -62,7 +62,7 @@ class ProductsTableSeeder extends Seeder
             Accusamus voluptatem ipsum veritatis fugiat eaque, facere qui nobis veniam sunt unde nulla rem obcaecati, sit repellendus. Est, suscipit voluptatibus earum cupiditate nihil qui labore? Molestias minus omnis velit odio!',
         ]);
 
-        Product::created([
+        Product::create([
             'name' => 'Mangue / Pomme',
             'slug' => 'mangue-Pomme',
             'details' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, numquam quia, voluptate mollitia nam dignissimos amet est tempore expedita illum ipsa delectus magni provident! Dignissimos autem vero itaque debitis aliquam.',
@@ -72,7 +72,7 @@ class ProductsTableSeeder extends Seeder
             Accusamus voluptatem ipsum veritatis fugiat eaque, facere qui nobis veniam sunt unde nulla rem obcaecati, sit repellendus. Est, suscipit voluptatibus earum cupiditate nihil qui labore? Molestias minus omnis velit odio!',
         ]);
 
-        Product::created([
+        Product::create([
             'name' => 'Mangue / Ananas',
             'slug' => 'mangue-Ananas',
             'details' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, numquam quia, voluptate mollitia nam dignissimos amet est tempore expedita illum ipsa delectus magni provident! Dignissimos autem vero itaque debitis aliquam.',
@@ -82,7 +82,7 @@ class ProductsTableSeeder extends Seeder
             Accusamus voluptatem ipsum veritatis fugiat eaque, facere qui nobis veniam sunt unde nulla rem obcaecati, sit repellendus. Est, suscipit voluptatibus earum cupiditate nihil qui labore? Molestias minus omnis velit odio!',
         ]);
 
-        Product::created([
+        Product::create([
             'name' => 'Mangue / Banane',
             'slug' => 'mangue-Banane',
             'details' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, numquam quia, voluptate mollitia nam dignissimos amet est tempore expedita illum ipsa delectus magni provident! Dignissimos autem vero itaque debitis aliquam.',
